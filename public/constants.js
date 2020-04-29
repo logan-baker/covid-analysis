75733114776429ddd306de1075d3716313d7e24a
Logan <lobaker13@gmail.com>
2020-04-29 15:48:55 -0400

    Update function declarations and comments

--- public/constants.js
+++ public/constants.js
@@ -1,3 +1,5 @@
+// Initial URLs to keep track.
+// ToDo: Update hardcoded values via interpolation
 export const COVID_URLS = {
     SUMMARY: 'https://api.covid19api.com/summary',
     STATES: 'https://corona.lmao.ninja/v2/states',
--- public/main.js
+++ public/main.js
@@ -5,18 +5,15 @@ const qSelect = function (q) {
 };
 
 const jsonButton = qSelect("#generate");
-const buttonContainer = qSelect("#buttonContainer");
 const display = qSelect("#displayContainer");
 const displayList = qSelect("#displayList");
 
 const collection = [
-    "Another",
-    "More",
+    "Click Me",
+    "Stats Please",
     "Next",
     "Continue",
-    "Keep going",
-    "Click me",
-    "A new one"
+    "Keep going"
 ];
 
 const generateJson = () => {
@@ -34,28 +31,29 @@ const generateJson = () => {
     console.log("CONSTANTS", url);
 };
 
-const formatJson = resJson => {
-    resJson = JSON.stringify(resJson);
-    let counter = 0;
-    console.log("Here's what it's rendering", JSON.parse(resJson));
-    return resJson
-        .split("")
-        .map(char => {
-            switch (char) {
-                case ",":
-                    return `,\n${" ".repeat(counter * 2)}`;
-                case "{":
-                    counter += 1;
-                    return `{\n${" ".repeat(counter * 2)}`;
-                case "}":
-                    counter -= 1;
-                    return `\n${" ".repeat(counter * 2)}}`;
-                default:
-                    return char;
-            }
-        })
-        .join("");
-};
+// Used initially for the full JSON object
+// const formatJson = resJson => {
+//     resJson = JSON.stringify(resJson);
+//     let counter = 0;
+//     console.log("Here's what it's rendering", JSON.parse(resJson));
+//     return resJson
+//         .split("")
+//         .map(char => {
+//             switch (char) {
+//                 case ",":
+//                     return `,\n${" ".repeat(counter * 2)}`;
+//                 case "{":
+//                     counter += 1;
+//                     return `{\n${" ".repeat(counter * 2)}`;
+//                 case "}":
+//                     counter -= 1;
+//                     return `\n${" ".repeat(counter * 2)}}`;
+//                 default:
+//                     return char;
+//             }
+//         })
+//         .join("");
+// };
 
 function calcMortRate(x, y) {
     return `${((x)/(y * .01)).toFixed(2)}%`;
@@ -78,14 +76,13 @@ const renderResponse = jsonResponse => {
         displayList.append(mdcListItem);
         mdcListItem.append(mdcListText);
     });
-    //   const jsonSelection = Math.floor(Math.random() * 10);
-    //   const testJson = JSON.parse(jsonResponse);
-      display.innerHTML = `<pre>${formatJson(jsonResponse)}</pre>`;
+    //   display.innerHTML = `<pre>${formatJson(jsonResponse)}</pre>`;
 };
 
-
+// ToDo: Update the DOM tree. Currently stacks data returned from the calls
+// One possibility is using `removeChild(this)` with the parent node reference then appending the child with updated data...data.or just use Angular...
 const changeButton = () => {
-    const newText = Math.floor(Math.random() * 7);
+    const newText = Math.floor(Math.random() * 5);
     jsonButton.innerHTML = `${collection[newText]}!`;
 };
 
