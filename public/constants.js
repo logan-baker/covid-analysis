3eb2317a76a1051f6ddcac542d88fa406ae34380
Logan <lobaker13@gmail.com>
2020-04-29 15:09:33 -0400

    Render data via the API and implement initial UI

---
+++ public/constants.js
@@ -0,0 +1,16 @@
+export const COVID_URLS = {
+    SUMMARY: 'https://api.covid19api.com/summary',
+    STATES: 'https://corona.lmao.ninja/v2/states',
+    NY: 'https://corona.lmao.ninja/v2/states/New York'
+}
+
+export const REQUEST_DATA = {
+    ACTIVE: 'active',
+    CASES: 'cases',
+    DEATHS: 'deaths',
+    STATE: 'state',
+    TESTS: 'tests',
+    TPM: 'testsPerOneMillion',
+    T_CASES: 'todayCases',
+    T_DEATHS: 'todayDeaths'
+}
---
+++ public/main.js
@@ -0,0 +1,95 @@
+import { COVID_URLS as url, REQUEST_DATA as data } from "./constants.js";
+
+const qSelect = function (q) {
+    return document.querySelector(q);
+};
+
+const jsonButton = qSelect("#generate");
+const buttonContainer = qSelect("#buttonContainer");
+const display = qSelect("#displayContainer");
+const displayList = qSelect("#displayList");
+
+const collection = [
+    "Another",
+    "More",
+    "Next",
+    "Continue",
+    "Keep going",
+    "Click me",
+    "A new one"
+];
+
+const generateJson = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+            changeButton();
+        }
+    };
+    xhr.open("GET", url.STATES);
+    xhr.send();
+    console.log("CONSTANTS", url);
+};
+
+const formatJson = resJson => {
+    resJson = JSON.stringify(resJson);
+    let counter = 0;
+    console.log("Here's what it's rendering", JSON.parse(resJson));
+    return resJson
+        .split("")
+        .map(char => {
+            switch (char) {
+                case ",":
+                    return `,\n${" ".repeat(counter * 2)}`;
+                case "{":
+                    counter += 1;
+                    return `{\n${" ".repeat(counter * 2)}`;
+                case "}":
+                    counter -= 1;
+                    return `\n${" ".repeat(counter * 2)}}`;
+                default:
+                    return char;
+            }
+        })
+        .join("");
+};
+
+function calcMortRate(x, y) {
+    return `${((x)/(y * .01)).toFixed(2)}%`;
+};
+
+const renderResponse = jsonResponse => {
+    jsonResponse.map(obj => {
+
+        const obS = obj.state;
+        const obC = obj.cases;
+        const obD = obj.deaths;
+
+        let mdcListItem = document.createElement("li");
+        let mdcListText = document.createElement("span");
+        mdcListItem.setAttribute("class", "mdc-list-item");
+        mdcListText.setAttribute("class", "mdc-list-item__text");
+        mdcListText.innerHTML = `
+        <b style="font-size: 20px">${obS}</b><span> (Total Cases: ${obC}) / (Total Deaths: ${obD}) --> Mortality Rate: ${calcMortRate(obD, obC)}</span>
+        `;
+        displayList.append(mdcListItem);
+        mdcListItem.append(mdcListText);
+    });
+    //   const jsonSelection = Math.floor(Math.random() * 10);
+    //   const testJson = JSON.parse(jsonResponse);
+      display.innerHTML = `<pre>${formatJson(jsonResponse)}</pre>`;
+};
+
+
+const changeButton = () => {
+    const newText = Math.floor(Math.random() * 7);
+    jsonButton.innerHTML = `${collection[newText]}!`;
+};
+
+jsonButton.addEventListener("click", generateJson);
+
+// Run local server with python avoid cors issue
+// ppy3 -m http.server --directory ~/demos/js_request_call 8001
